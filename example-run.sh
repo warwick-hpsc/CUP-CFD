30308fa93b223ac1b9ca88c1b0b1714dd3b4299f
Andrew Owenson <aowenson@protonmail.ch>
2020-05-31 21:07:03 +0100

    Better example-run.sh

--- example-run.sh
+++ example-run.sh
@@ -6,12 +6,21 @@ cd examples
 
 ## Currently, cup-cfd loads a hard-coded json file:
 # ln -sf examplejson.json cupcfd.json
-ln -sf particlesOnGen.simple.json cupcfd.json
+# ln -sf particlesOnGen.json cupcfd.json
+ln -sf particlesOnGen-large.json cupcfd.json
 
 if [ -d tt_results ]; then
 	rm -r tt_results
 fi
 
-# mpirun -n 2 xterm -fa 'Monospace' -fs 14 -e gdb ../build/cupcfd
-mpirun -n 2 xterm -fa 'Monospace' -fs 12 -e gdb ../build/cupcfd
+# _n=3
+_n=4
 
+# mpirun -n ${_n} xterm -fa 'Monospace' -fs 14 -e gdb ../build/cupcfd
+# mpirun -n ${_n} xterm -fa 'Monospace' -fs 12 -e gdb ../build/cupcfd
+mpirun -n ${_n} ../build/cupcfd
+
+if [ -d "run-n${_n}-tt_results" ]; then
+	rm -r "run-n${_n}-tt_results"
+fi
+mv tt_results "run-n${_n}-tt_results"
