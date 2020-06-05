f171ba78889291775d6711e9f77b0d995d9545fa
Andrew Owenson <aowenson@protonmail.ch>
2020-06-05 21:58:24 +0100

    Improved post-processing script to create per-rank and rank-sum charts

--- scripts/treetimer-postprocessing.py
+++ scripts/treetimer-postprocessing.py
@@ -27,6 +27,7 @@
 
 import sys, os, re
 from pprint import pprint
+from copy import deepcopy
 
 import sqlite3
 
@@ -38,53 +39,133 @@ import matplotlib.patches as mpatches
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--tt-results-dirpath', required=True, help="Dirpath to 'tt_results' folder")
+# parser.add_argument('-c', '--charts', help="Chart call stack runtime breakdown for each rank")
+parser.add_argument('-c', '--charts', choices=["polar", "horizontal", "vertical"])
 args = parser.parse_args()
 
+
+import enum
+class PlotType(enum.Enum):
+	Polar = 1
+	Horizontal = 2
+	Vertical = 3
+plotTypeToString = {PlotType.Polar:"Polar", PlotType.Horizontal:"Horizontal", PlotType.Vertical:"Vertical"}
+
+
+
+methodTypeToColour = {}
+methodTypeToColour["Program"] = "silver"
+methodTypeToColour["Method"] = "silver"
+methodTypeToColour["Block"] = "silver"
+methodTypeToColour["Loop"] = "fuchsia"
+methodTypeToColour["Compute"] = "fuchsia"
+methodTypeToColour["MPICommCall"] = "aqua"
+methodTypeToColour["MPISyncCall"] = "orange"
+methodTypeToColour["LibraryCall"] = "yellowgreen"
+
 def main():
+
 	tt_folder_dirpath = args.tt_results_dirpath
 	if not os.path.isdir(tt_folder_dirpath):
 		raise Exception("Folder does not exist: {0}".format(tt_folder_dirpath))
+
 	df_all = None
+
 	rank_ids = set()
+
+	## Group together rank call traces that have identical topology:
+	groupedCallTrees = None
+
 	for run_root, run_dirnames, run_filenames in os.walk(tt_folder_dirpath):
 		for f in run_filenames:
 			m = re.match("^results\.([0-9]+)\.db$", f)
 			if m:
-				rank = m.groups()[0]
-				# rank_ids.add(rank)
-				# df = load_db(os.path.join(tt_folder_dirpath, f))
-				# df["rank"] = rank
-				# if df_all is None:
-				# 	df_all = df
-				# else:
-				# 	df_all = df_all.append(df)
+				rank = int(m.groups()[0])
+				rank_ids.add(rank)
+				df = load_db(os.path.join(tt_folder_dirpath, f))
+				df["rank"] = rank
+				if df_all is None:
+					df_all = df
+				else:
+					df_all = df_all.append(df)
 
 
 				db = sqlite3.connect(os.path.join(tt_folder_dirpath, f))
 				## Function 1
 				## Display Hotspots by Grouped Node Type Exclusive Time
 				## This will group all of the nodes of specific types - e.g. MPI call, compute etc.
-				## UPDATE: Function 1 does not behave as described.
 				# printSummaryExclusiveTimeByType(1, 1, db)
-
-				## Function 2
-				## Display Full CallTree (All CallPath Nodes) alongside exclusive time spent in each function
-				## Note: There is no guarantee that the ordering is in order of first called (this is only
-				## tracked in trace mode), but all blocks will be correctly nested under blocks from which they were
-				## called. An indention indicates a nested block inside another block.
-				# printAggregateDetailsCallPathTree(1, 1, db)
-				t = buildCallPathTree(1, 1, db)
-				# pprint(t)
+				# rDict = getSummaryExclusiveTimeByType(1, 1, db)
+				# print(rDict)
 				# quit()
 
-				fig_dims = (4,4)
-				fig = plt.figure(figsize=fig_dims)
-				# sunburst(t)
-				sunburst_root(t)
-				plt.savefig("chart.png")
-				plt.close(fig)
+				if not args.charts is None:
+					t = buildCallPathTree(1, 1, db)
 
-				quit()
+					## Add tree to a group
+					if groupedCallTrees is None:
+						groupedCallTrees = [ {rank:t} ]
+					else:
+						found_group = False
+						for treeGroup in groupedCallTrees:
+							tOther = next(iter(treeGroup.values()))
+							if t == tOther:
+								treeGroup[rank] = t
+								found_group = True
+						if not found_group:
+							groupedCallTrees.append({rank:t})
+
+					# # Plot this call tree
+					# if args.charts == "polar":
+					# 	plotType = PlotType.Polar
+					# elif args.charts == "vertical":
+					# 	plotType = PlotType.Vertical
+					# else:
+					# 	plotType = PlotType.Horizontal
+
+					# fig_dims = (16,16)
+					# fig = plt.figure(figsize=fig_dims)
+					# plotCallPath_root(t, plotType)
+
+					# fig.suptitle("Call stack times of rank " + str(rank))
+					# chart_dirpath = os.path.join(tt_folder_dirpath, "charts", plotTypeToString[plotType])
+					# if not os.path.isdir(chart_dirpath):
+					# 	os.makedirs(chart_dirpath)
+					# chart_filepath = os.path.join(chart_dirpath, "rank-{0}.png".format(rank))
+					# plt.savefig(chart_filepath)
+					# plt.close(fig)
+
+	## Aggregate together call trees within each group, and create plots:
+	aggregatedCallTrees = []
+	for g in groupedCallTrees:
+		agg = None
+		for rank,t in g.items():
+			if agg is None:
+				agg = CallTreeNodeAggregated(t, rank)
+			else:
+				agg.appendNodeElementwise(t, rank)
+		aggSum = agg.sumElementwise()
+
+		if args.charts == "polar":
+			plotType = PlotType.Polar
+		elif args.charts == "vertical":
+			plotType = PlotType.Vertical
+		else:
+			plotType = PlotType.Horizontal
+
+		fig_dims = (16,16)
+		fig = plt.figure(figsize=fig_dims)
+		plotCallPath_root(aggSum, plotType)
+
+		fig.suptitle("Call stack times summed across ranks " + sorted(agg.ranks).__str__())
+		chart_dirpath = os.path.join(tt_folder_dirpath, "charts", plotTypeToString[plotType])
+		if not os.path.isdir(chart_dirpath):
+			os.makedirs(chart_dirpath)
+		chart_filepath = os.path.join(chart_dirpath, "ranksSum.png".format(rank))
+		plt.savefig(chart_filepath)
+		plt.close(fig)
+
+
 
 	df_all["num_ranks"] = len(rank_ids)
 
@@ -92,9 +173,9 @@ def main():
 	df_all.sort_values(["Name", "rank"], inplace=True)
 
 	# df_filename = "hotspotsAggregateExclusiveTime.csv"
-	df_filename = os.path.join(tt_folder_dirpath, "hotspotsAggregateExclusiveTime.csv")
+	df_filename = os.path.join(tt_folder_dirpath, "timings.csv")
 	df_all.to_csv(df_filename, index=False)
-	# print("Data written to '{0}'".format(df_filename))
+	print("Data written to '{0}'".format(df_filename))
 
 def load_db(db_filepath):
 
@@ -223,34 +304,127 @@ def getRootCallPathID(db):
 	return result['CallPathID']
 
 
-# Traverse the call path tree for a run, and print the aggregate details for each
-# call path node
-# Indented to show depth of call in stack
-# Note - RunID is the SQL Key for the RunID, and processID is the SQL Key ProcessID in the db, not the process rank! (They may not be the same)
-def printAggregateDetailsCallPathTree(runID, processID, db):
 
-	# Start at root
-	rootID = getRootCallPathID(db)
+class CallTreeNodeIterator:
+	def __init__(self, callTreeNode, am_root=False):
+		self.callTreeNode = callTreeNode
+		self.callTreeNodeLeaves = sorted(callTreeNode.leaves)
+		self.currentLeafIter = None
+		self.index = -1
+		self.am_root = am_root
 
-	print('Depth & ID ,Name ,TypeName ,AggMinTime ,AggAvgTime ,AggMaxTime ,CallCount ,AggTotalTimeI ,AggTotalTimeE')
-	printAggregateDetailsCallPathNodeTraversal(runID, rootID, processID, db, 0)
+	def __next__(self):
+		n = None
+		if self.index == -1:
+			n = self.callTreeNode.time
+			# n = "{0} : {1}".format(self.callTreeNode.name, self.callTreeNode.time)
+			self.index += 1
+		else:
+			if len(self.callTreeNodeLeaves) > 0:
+				while self.index < len(self.callTreeNodeLeaves):
+					if self.currentLeafIter is None:
+						self.currentLeafIter = self.callTreeNodeLeaves[self.index].__iter__()
+					n = self.currentLeafIter.__next__()
+					if not n is None:
+						break
+					self.index += 1
+					self.currentLeafIter = None
 
-def printAggregateDetailsCallPathNodeTraversal(runID, callPathID, processID, db, indentLevel):
-	# Recursive method to do depth first traversal when printing nodes
+		if n is None and self.am_root:
+			raise StopIteration
+		return n
 
-	for i in xrange(0, indentLevel):
-		sys.stdout.write('  ')
+class CallTreeNode:
+	def __init__(self, name, typeName, time):
+		self.name = name
+		self.typeName = typeName
+		self.time = time
+		self.leaves = []
 
-#	getAggregateCallPathNodeDetails(callPathID, runID, processID, db)
-	sys.stdout.write(str(callPathID) + ",")
-	printAggregateCallPathNodeDetails(callPathID, runID, processID, db)
-	childNodes = getCallPathChildNodeIDs(callPathID, db)
+	def addLeaf(self, leaf):
+		self.leaves.append(leaf)
 
-	# Loop over child nodes
-	for childID in childNodes:
-		# Keep following a path till we reach a node with no children
-		printAggregateDetailsCallPathNodeTraversal(runID, childID, processID, db, indentLevel + 1)
+	## Comparison operators for tree structure, ignoring time:
+	def __lt__(self, other):
+		if self.name != other.name:
+			return self.name < other.name
+		if self.typeName != other.typeName:
+			return self.typeName < other.typeName
+		return self.leaves < other.leaves
 
+	def __eq__(self, other):
+		if self.name != other.name:
+			return False
+		if self.typeName != other.typeName:
+			return False
+		return self.leaves == other.leaves
+
+	def __str__(self, indent=0):
+		nodeStr = " "*indent + "{0} [{1}] - {2:.2f} seconds\n".format(self.name, self.typeName, self.time)
+		for c in self.leaves:
+			cStr = c.__str__(indent+1)
+			nodeStr += cStr
+		return nodeStr
+
+	def __add__(self, other):
+		if not self == other:
+			raise Exception("Tree structures not identical")
+		summed = CallTreeNode(self.name, self.typeName, self.time)
+		summed.time += other.time
+		for idx in range(len(self.leaves)):
+			summed.addLeaf(self.leaves[idx] + other.leaves[idx])
+		return summed
+
+	def __iter__(self, am_root=False):
+		return CallTreeNodeIterator(self, self.name=="ProgramRoot")
+
+class CallTreeNodeAggregated:
+	def __init__(self, node, rank):
+		if not isinstance(node, CallTreeNode):
+			raise Exception("Must initialise CallTreeNodeAggregated with a CallTreeNode")
+
+		self.name = node.name
+		self.typeName = node.typeName
+		self.times = [node.time]
+		self.leaves = []
+		self.init_node_copy = deepcopy(node)
+		self.ranks = [rank]
+		for l in sorted(node.leaves):
+			self.leaves.append(CallTreeNodeAggregated(l, rank))
+
+	def appendNodeElementwise(self, node, rank):
+		if not isinstance(node, CallTreeNode):
+			raise Exception("Addition must be a CallTreeNode")
+
+		if self.init_node_copy != node:
+			raise Exception("Attempting to add node to CallTreeNodeAggregated with different topology")
+
+		self.times.append(node.time)
+		self.ranks.append(rank)
+		nodeLeavesSorted = sorted(node.leaves)
+		for idx in range(len(self.leaves)):
+			self.leaves[idx].appendNodeElementwise(nodeLeavesSorted[idx], rank)
+
+	def sumElementwise(self):
+		timesSum = sum(self.times)
+		sumNode = CallTreeNode(self.name, self.typeName, timesSum)
+		for l in self.leaves:
+			sumNode.addLeaf(l.sumElementwise())
+		return(sumNode)
+
+	def __str__(self, indent=0):
+		timesStr = "[{0:.2f}".format(self.times[0])
+		for i in range(1,len(self.times)):
+			timesStr += ", {0:.2f}".format(self.times[i])
+		timesStr += "]"
+		# nodeStr = " "*indent + "{0} [{1}] - {2}\n".format(self.name, self.typeName, timesStr)
+		nodeStr = " "*indent + "{0} [{1}]:\n".format(self.name, self.typeName)
+		nodeStr+= " "*indent + "  times: {0}\n".format(timesStr)
+		nodeStr+= " "*indent + "  ranks: {0}\n".format(self.ranks.__str__())
+		for c in self.leaves:
+			cStr = c.__str__(indent+1)
+			nodeStr += cStr
+		return nodeStr
 
 def buildCallPathTree(runID, processID, db):
 	rootID = getRootCallPathID(db)
@@ -262,20 +436,21 @@ def buildCallPathNodeTraversal(runID, processID, db, treeNode, nodeID, indentLev
 	# Recursive depth-first traversal through call stack
 	record = getAggregateCallPathNodeDetails(nodeID, runID, processID, db)
 
-	# leaf = [record["Name"], record["TypeName"], record["AggTotalTimeI"], [] ]
-	# leaf = [ ( record["Name"], record["TypeName"], record["AggTotalTimeI"], [] ) ]
-	leaf = ( record["Name"], record["TypeName"], record["AggTotalTimeI"], [] )
+	# leaf = ( record["Name"], record["TypeName"], record["AggTotalTimeI"], [] )
+	# am_root = False
+	# if treeNode is None:
+	# 	am_root = True
+	# 	treeNode = [ leaf ]
+	# else:
+	# 	treeNode[-1].append(leaf)
+
+	leaf = CallTreeNode(record["Name"], record["TypeName"], record["AggTotalTimeI"])
 	am_root = False
 	if treeNode is None:
 		am_root = True
-		# treeNode = leaf
-		treeNode = [ leaf ]
+		treeNode = leaf
 	else:
-		# pprint(treeNode)
-		treeNode[-1].append(leaf)
-		# treeNode[0][-1].append(leaf)
-		# treeNode[0][-1].append([leaf])
-		# treeNode[-1].append([leaf])
+		treeNode.addLeaf(leaf)
 
 	childNodes = getCallPathChildNodeIDs(nodeID, db)
 	for childID in childNodes:
@@ -283,7 +458,6 @@ def buildCallPathNodeTraversal(runID, processID, db, treeNode, nodeID, indentLev
 
 	if am_root:
 		return treeNode
-
 
 def getCallPathChildNodeIDs(callPathID, db):
 	db.row_factory = sqlite3.Row
@@ -301,58 +475,57 @@ def getCallPathChildNodeIDs(callPathID, db):
 
 	return list
 
-# do_plt_polar = True
-do_plt_polar = False
-
-do_bar_plot_vertical = True
-# do_bar_plot_vertical = False
-
-methodTypeToColour = {}
-methodTypeToColour["Program"] = "silver"
-methodTypeToColour["Method"] = "silver"
-methodTypeToColour["Loop"] = "silver"
-methodTypeToColour["ComputeLoop"] = "purple"
-methodTypeToColour["MPICommCall"] = "blue"
-methodTypeToColour["MPISyncCall"] = "orange"
-
-def sunburst_root(nodes):
-	if do_plt_polar:
+def plotCallPath_root(tree, plotType):
+	if plotType == PlotType.Polar:
 		root_total = np.pi * 2
 	else:
-		label, methodType, value, subnodes = nodes[0]
-		root_total = value
-		print("root_total = {0}".format(root_total))
+		# label, methodType, value, subnodes = nodes[0]
+		# root_total = value
+		root_total = tree.time
 
-	if do_plt_polar:
+	if plotType == PlotType.Polar:
 		ax = plt.subplot(111, projection='polar')
 	else:
 		ax = plt.subplot(111)
 
-	sunburst(nodes, root_total, root_total, 0, 0, ax)
+	plotCallPath(tree, root_total, root_total, 0, 0, ax, plotType)
 
-# def sunburst(nodes, total=root_total, offset=0, level=0, ax=None):
-def sunburst(nodes, root_total, node_total, offset, level, ax):
-	if level == 0 and len(nodes) > 1:
-		pprint(nodes)
-		raise Exception("There must only be one root node, not {0}".format(len(nodes)))
+def plotCallPath(tree, root_total, node_total, offset, level, ax, plotType):
+	# if level == 0 and len(nodes) > 1:
+	# 	pprint(nodes)
+	# 	raise Exception("There must only be one root node, not {0}".format(len(nodes)))
 
-	if level == 0 and len(nodes) == 1:
-		label, methodType, value, subnodes = nodes[0]
+	# if level == 0 and len(nodes) == 1:
+	# 	label, methodType, value, subnodes = nodes[0]
+	if level == 0:
+		if not isinstance(tree, CallTreeNode):
+			raise Exception("'tree' parameter at root must be a CallTreeNode")
+		label = tree.name
+		methodType = tree.typeName
+		value = tree.time
+		subnodes = tree.leaves
 
-		if do_plt_polar:
+		if plotType == PlotType.Polar:
 			ax.bar(x=[0], height=[0.5], width=[node_total], color=[methodTypeToColour[methodType]])
 			ax.text(0, 0, label, ha='center', va='center')
 		else:
-			if do_bar_plot_vertical:
+			if plotType == PlotType.Vertical:
 				ax.bar(x=[node_total/2], height=[1.0], width=[node_total], color=[methodTypeToColour[methodType]])
 				ax.text(root_total/2, 0.5, label, ha='center', va='center')
 			else:
 				ax.bar(x=[0.5], height=[node_total], width=[1.0], color=[methodTypeToColour[methodType]])
 				ax.text(0.5, root_total/2, label, ha='center', va='center')
 
-		sunburst(subnodes, root_total, value, 0, level+1, ax)
-	elif nodes:
-		if do_plt_polar:
+		plotCallPath(subnodes, root_total, value, 0, level+1, ax, plotType)
+	elif tree:
+		if not isinstance(tree, list):
+			raise Exception("non-root 'tree' must be a list of CallTreeNode")
+		for n in tree:
+			if not isinstance(n, CallTreeNode):
+				raise Exception("non-root 'tree' must be a list of CallTreeNode")
+
+		# if do_plt_polar:
+		if plotType == PlotType.Polar:
 			d = root_total / node_total
 		else:
 			d = root_total / node_total
@@ -360,23 +533,29 @@ def sunburst(nodes, root_total, node_total, offset, level, ax):
 		labels = []
 		colours = []
 		subnode_offset = offset
-		# for label, value, subnodes in nodes:
-		for label, methodType, value, subnodes in nodes:
-			if (value / node_total) < 0.03:
-				## Prune out small nodes
-				continue
-			labels.append(label)
+		# for label, methodType, value, subnodes in nodes:
+		for t in tree:
+			label = t.name
+			methodType = t.typeName
+			value = t.time
+			subnodes = t.leaves
+
+			if (value / node_total) < 0.01:
+				## No label text for tiny segments
+				labels.append("")
+			else:
+				labels.append(label)
 			sizes.append(value * d)
 			colours.append(methodTypeToColour[methodType])
-			sunburst(subnodes, root_total, node_total, subnode_offset, level+1, ax)
+			plotCallPath(subnodes, root_total, node_total, subnode_offset, level+1, ax, plotType)
 			subnode_offset += value
-		if do_plt_polar:
+		if plotType == PlotType.Polar:
 			widths = sizes
-			heights = [1] * len(nodes)
+			heights = [1] * len(sizes)
 			lefts = np.cumsum([offset * d] + widths[:-1])
-			bottoms = np.zeros(len(nodes)) + level - 0.5
+			bottoms = np.zeros(len(sizes)) + level - 0.5
 		else:
-			if do_bar_plot_vertical:
+			if plotType == PlotType.Vertical:
 				heights = [1] * len(sizes)
 				widths = sizes
 				bottoms = np.zeros(len(sizes)) + level
@@ -391,43 +570,40 @@ def sunburst(nodes, root_total, node_total, offset, level, ax):
 		for rect, label in zip(rects, labels):
 			x = rect.get_x() + rect.get_width() / 2
 			y = rect.get_y() + rect.get_height() / 2
-			if do_plt_polar:
-				rotation = (90 + (360 - np.degrees(x) % 180)) % 360
-				ax.text(x, y, label, rotation=rotation, ha='center', va='center')
+			if plotType == PlotType.Polar:
+				do_rotate_text = True
+				if do_rotate_text:
+					if level < 2:
+						rotation = ((360 - np.degrees(x) % 180)) % 360
+					else:
+						rotation = (90 + (360 - np.degrees(x) % 180)) % 360
+					ax.text(x, y, label, rotation=rotation, ha='center', va='center')
+				else:
+					ax.text(x, y, label, ha='center', va='center')
 			else:
 				ax.text(x, y, label, ha='center', va='center')
 
 	if level == 0:
-		if do_plt_polar:
+		if plotType == PlotType.Polar:
 			ax.set_theta_direction(-1)
 			ax.set_theta_zero_location('N')
 			ax.set_axis_off()
 		else:
-			if do_bar_plot_vertical:
+			if plotType == PlotType.Vertical:
 				ax.get_yaxis().set_visible(False)
 			else:
 				ax.get_xaxis().set_visible(False)
 
 		## Build custom colour legend:
-		legend_dict = { "MPI sync" : methodTypeToColour["MPISyncCall"],  "compute" : methodTypeToColour["ComputeLoop"] }
+		legend_dict = { "MPI sync" : methodTypeToColour["MPISyncCall"], 
+		                "MPI comm" : methodTypeToColour["MPICommCall"], 
+		                "compute" : methodTypeToColour["Compute"],
+		                "library call" : methodTypeToColour["LibraryCall"] }
 		patchList = []
 		for k in legend_dict:
 			patchList.append(mpatches.Patch(color=legend_dict[k], label=k))
 		ax.legend(handles=patchList)
 
-
-def printAggregateCallPathNodeDetails(callPathID, runID, processID, db):
-
-	record = getAggregateCallPathNodeDetails(callPathID, runID, processID, db)
-
-	print(str(record['Name']) + "," + \
-		  str(record['TypeName']) + "," + \
-		  str(record['AggMinTime']) + "," + \
-		  str(record['AggAvgTime']) + "," + \
-		  str(record['AggMaxTime']) + "," + \
-		  str(record['CallCount']) + "," + \
-		  str(record['AggTotalTimeI']) + "," + \
-		  str(record['AggTotalTimeE']))
 
 def getAggregateCallPathNodeDetails(callPathID, runID, processID, db):
 	# This method retrieves the following aggregate details about a node and returns them as a dictionary
@@ -509,6 +685,9 @@ def getAggregateCallPathNodeDetails(callPathID, runID, processID, db):
 
 def printSummaryExclusiveTimeByType(runID, processID, db):
 	rDict = getSummaryExclusiveTimeByType(runID, processID, db)
+
+	print(rDict)
+	quit()
 
 	print("==============================================")
 	print("Summary: Exclusive Time in code blocks by type")
